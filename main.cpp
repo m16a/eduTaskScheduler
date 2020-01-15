5ecaab15961d0cc6d72ac9c90793cc88757f9b3d
m16a <mshvorak@gmail.com>
2020-01-16 00:53:01 +0200

    extremly naive implementation of task dependency

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <unordered_set>
 #include <thread>
 #include <queue>
 #include <iostream>
@@ -21,16 +22,25 @@
 #define EASY_FUNCTION(a)
 #endif
 
+using TID = int;
+
 struct ITask
 {
 	virtual	void Call() = 0;
+	virtual	TID GetTID() = 0;
+	virtual	void SetTID(TID tid) = 0;
+	virtual	bool IsDependOn(TID tid) = 0;
+	virtual	bool HasDependentOn() = 0;
+	virtual	void RemoveDependentOn(TID tid) = 0;
+	virtual	void AddDependentOn(TID tid) = 0;
+
 	virtual ~ITask() {}
 };
 
 template <typename F>
 struct CTask : public ITask
 {
-    CTask (std::function<F>&& f) : pack(f) {}
+    CTask (std::function<F>&& f) : pack(f), tid(0) {}
     std::packaged_task<F>	pack;
 
     virtual void Call() override
@@ -38,6 +48,36 @@ struct CTask : public ITask
         pack();
     }
 
+	virtual	TID GetTID() override { return tid; }
+	virtual void SetTID(TID id) override {
+		tid = id;
+	}
+
+	virtual	bool IsDependOn(TID tid) override {
+		if (dependsOn.empty())
+			return false;
+
+		if (dependsOn.find(tid) != dependsOn.end())
+			return true;
+
+		return false;
+	}
+	
+	virtual	void RemoveDependentOn(TID tid) override {
+		auto it = dependsOn.find(tid);
+		dependsOn.erase(it);
+	}
+	
+	virtual	void AddDependentOn(TID tid) override {
+		dependsOn.insert(tid);
+	}
+
+	virtual	bool HasDependentOn() override {
+		return !dependsOn.empty();
+	}
+
+	TID tid;
+	std::unordered_set<TID> dependsOn;
 };
 
 double Payload()
@@ -66,9 +106,10 @@ float Payload2()
 class TaskScheduler
 {
 	public:
-	TaskScheduler(unsigned int n = std::thread::hardware_concurrency()) : m_ThreadsCount(n)
+	TaskScheduler(unsigned int n = std::thread::hardware_concurrency()) : m_ThreadsCount(n), m_last_tid(0)
 	{
 		stop = false;
+		start = false;
 		RunAllThreads();
 	}
 
@@ -82,36 +123,85 @@ class TaskScheduler
 		}	
 	}
 
-	void AddTask(std::shared_ptr<ITask>& t)
+	TID AddTask(std::shared_ptr<ITask>& t)
 	{
 		std::lock_guard<std::mutex> lock(m_mutex);
-		m_tasks.push(t);
+		
+		TID tid = 0;
+		if (m_free_tids.empty()) {
+			tid = ++m_last_tid;
+		}
+		else {
+			tid = m_free_tids.back();
+			m_free_tids.pop_back();
+		}
+		t->SetTID(tid);
+
+		m_tasks.push_back(t);
 
 		newTask.notify_one();
+
+		return tid;
+	}
+
+	TID AddTask(std::shared_ptr<ITask>& t, std::vector<TID> deps)
+	{
+		std::lock_guard<std::mutex> lock(m_mutex);
+		
+		TID tid = 0;
+		if (m_free_tids.empty()) {
+			tid = ++m_last_tid;
+		}
+		else {
+			tid = m_free_tids.back();
+			m_free_tids.pop_back();
+		}
+		t->SetTID(tid);
+
+		for (auto tid : deps) {
+			t->AddDependentOn(tid);
+		}
+
+		m_tasks.push_back(t);
+
+		newTask.notify_one();
+
+		return tid;
 	}
 
 	std::shared_ptr<ITask> PopTask()
 	{
 		std::lock_guard<std::mutex> lock(m_mutex);
-		
-		std::shared_ptr<ITask> t = m_tasks.front();		
-		m_tasks.pop();
-		return t;
+
+		std::shared_ptr<ITask> task_to_call;
+		for (int i = 0, size = m_tasks.size(); i < size; i++) {
+			if (!m_tasks[i]->HasDependentOn()) {
+				task_to_call = m_tasks[i];
+				std::swap(m_tasks[i], m_tasks[size-1]);
+				m_tasks.pop_back();
+				break;
+			}
+		}
+		return task_to_call;
 	}
 
 	size_t TasksCount()
 	{
 		return m_tasks.size(); //not thread safe;
 	}
+
+	void StartExecuting() {
+		start = true;
+	}
 	
 	private:
-	std::queue<std::shared_ptr<ITask>> m_tasks;
+	std::vector<std::shared_ptr<ITask>> m_tasks;
 	std::vector<std::thread> m_threads;
 
 	const unsigned int m_ThreadsCount;
 	void RunAllThreads()
 	{
-		for (int i=0; i<m_ThreadsCount; i++)
+		for (int i=0; i < m_ThreadsCount; i++)
 		{
 			m_threads.emplace_back(std::thread(&TaskScheduler::RunThread, this));
 		}	
@@ -119,8 +209,10 @@ class TaskScheduler
 
 	void RunThread()
 	{
-		std::cout << "run" << std::endl;
+		//std::cout << "run" << std::endl;
 		
+		while (!start) {}
+
 		while (!stop)
 		{
 			std::unique_lock<std::mutex> lock(m_mutex2);
@@ -128,14 +220,34 @@ class TaskScheduler
 			std::shared_ptr<ITask> pT = PopTask();
 			lock.unlock();
 
-			pT->Call();
+			if (pT) {
+				const TID tid = pT->GetTID();
+
+				if (tid) {
+					pT->Call();
+
+					for (auto& t : m_tasks) {
+						if (t->IsDependOn(tid)) {
+							t->RemoveDependentOn(tid);
+						}
+					}
+
+					newTask.notify_one();
+				}
+			}
+
+
 		}	
 	}
 
 	std::condition_variable newTask;
 	std::atomic<bool> stop;
+	std::atomic<bool> start;
 	std::mutex m_mutex;
 	std::mutex m_mutex2;
+
+	std::vector<TID> m_free_tids;
+	TID m_last_tid;
 };
 
 int main()
@@ -146,6 +258,7 @@ int main()
 
 	TaskScheduler ts;
 
+	/*
 	for (int i=0; i<20000; ++i)
 	{
 	    auto lambdaFunc = [](){Payload();};
@@ -169,10 +282,53 @@ int main()
 
 		//future.get(); //wait all task, comment to skip
 	}
+*/
+
+	TID tid1;
+	{
+		auto lambdaFunc = []() { std::cout << "Task 1" << std::endl; };
+		auto pT = new CTask<void()>(lambdaFunc);
+		auto shared = std::shared_ptr<ITask>(pT);
+		tid1 = ts.AddTask(shared);
+	}
+	
+	TID tid2;
+	{
+		auto lambdaFunc = []() { std::cout << "Task 2" << std::endl; };
+		auto pT = new CTask<void()>(lambdaFunc);
+		auto shared = std::shared_ptr<ITask>(pT);
+		tid2 = ts.AddTask(shared);
+	}
+	
+	TID tid3;
+	{
+		auto lambdaFunc = []() { std::cout << "Task 3" << std::endl; };
+		auto pT = new CTask<void()>(lambdaFunc);
+		auto shared = std::shared_ptr<ITask>(pT);
+		tid3 = ts.AddTask(shared, {tid1, tid2});
+	}
+
+	TID tid4;
+	{
+		auto lambdaFunc = []() { std::cout << "Task 4" << std::endl; };
+		auto pT = new CTask<void()>(lambdaFunc);
+		auto shared = std::shared_ptr<ITask>(pT);
+		tid4 = ts.AddTask(shared);
+	}
+
+	TID tid5;
+	{
+		auto lambdaFunc = []() { std::cout << "Task 5" << std::endl; };
+		auto pT = new CTask<void()>(lambdaFunc);
+		auto shared = std::shared_ptr<ITask>(pT);
+		tid5 = ts.AddTask(shared, {tid3, tid4 });
+	}
+
+	ts.StartExecuting();
 
 	while (true)
 	{
-		std::cout << "Tasks: " << ts.TasksCount() << std::endl;
+		std::cout << "tsks cnt: " << ts.TasksCount() << std::endl;
 
 #ifdef WIN32
 		Sleep(1000);
